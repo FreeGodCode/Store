61da96983d8e7165a00ff6a55827f0ae223f21d4
FreeGodCode <2501160661@qq.com>
2021-03-04 22:37:43 +0800

    添加入库路由配置

--- storein/urls.py
+++ storein/urls.py
@@ -7,5 +7,11 @@ from django.conf.urls import url
 from . import views
 
 patterns = [
-    url(r'^', views.LoginView.as_view(), name=''),
+    url(r'^buy_in_stores', views.BuyInStoresView.as_view(), name='buy_in_stores'),
+    url(r'^buy_in_store_new', views.BuyInStoreNewView.as_view(), name='buy_in_store_new'),
+    url(r'^buy_in_store_update', views.BuyInStoreUpdateView.as_view(), name='buy_in_store_update'),
+    url(r'^pOChoice', views.POChoiceView.as_view(), name='pOChoice'),
+    url(r'^buy_in_store_detail_save', views.BuyInStoreDetailSaveView.as_view(), name='buy_in_store_detail_save'),
+    url(r'^buy_in_store_detail_submit', views.BuyInStoreDetailSubmitView.as_view(), name='buy_in_store_detail_submit'),
+    url(r'^buy_in_store_delete', views.BuyInStoreDeleteView.as_view(), name='buy_in_store_delete')
 ]
